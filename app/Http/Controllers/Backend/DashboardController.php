33429fbffed3ae10fd0d8055c5796417fcdd4d81
abdrahman02 <abdul.190180078@mhs.unimal.ac.id>
2023-09-20 18:28:00 +0700

    halaman overview role admin

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\Berita;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Pengaduan;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
@@ -12,6 +15,61 @@ class DashboardController extends Controller
     {
         $user = Auth::user();
         $title = 'Overview';
-        return view('backend.index', compact('title', 'user'));
+
+        $bulanTahunPertama = null;
+        $bulanTahunTerbaru = null;
+        $jumlah_berita = 0;
+        $bulanTahunOnProcessPertama = null;
+        $bulanTahunOnProcessTerbaru = null;
+        $jumlah_OnProcess = 0;
+        $bulanTahunSelesaiPertama = null;
+        $bulanTahunSelesaiTerbaru = null;
+        $jumlah_Selesai = 0;
+        $bulanTahunPelanggan = null;
+        $bulanTahunPelanggan = null;
+        $jumlah_Pelanggan = 0;
+
+        // BERITA
+        // Mendapatkan data pertama
+        $dataPertama = Berita::orderBy('created_at')->first();
+        $bulanTahunPertama = date('F Y', strtotime($dataPertama->created_at));
+        // Mendapatkan data terbaru
+        $dataTerbaru = Berita::orderBy('created_at', 'desc')->first();
+        $bulanTahunTerbaru = date('F Y', strtotime($dataTerbaru->created_at));
+        // jumlah data
+        $jumlah_berita = Berita::count();
+
+
+        $dataOnProcessPertama = Pengaduan::where('progres', 'Sedang Proses')->orderBy('created_at')->first();
+        $dataSelesaiPertama = Pengaduan::where('progres', 'Selesai')->orderBy('created_at')->first();
+        // Data Sedang Proses
+        // Mendapatkan data pertama
+        if (isset($dataOnProcessPertama)) {
+            $bulanTahunOnProcessPertama = date('F Y', strtotime($dataOnProcessPertama->created_at));
+            // Mendapatkan data terbaru
+            $dataOnProcessTerbaru = Pengaduan::where('progres', 'Sedang Proses')->orderBy('created_at', 'desc')->first();
+            $bulanTahunOnProcessTerbaru = date('F Y', strtotime($dataOnProcessTerbaru->created_at));
+            // Jumlah data
+            $jumlah_OnProcess = Pengaduan::where('progres', 'Sedang Proses')->count();
+        } elseif ($dataSelesaiPertama) {
+            // Data Selesai
+            // Mendapatkan data terbaru
+            $bulanTahunSelesaiPertama = date('F Y', strtotime($dataSelesaiPertama->created_at));
+            $dataSelesaiTerbaru = Pengaduan::where('progres', 'Selesai')->orderBy('created_at', 'desc')->first();
+            $bulanTahunSelesaiTerbaru = date('F Y', strtotime($dataSelesaiTerbaru->created_at));
+            // Jumlah data
+            $jumlah_Selesai = Pengaduan::where('progres', 'Selesai')->count();
+        }
+
+
+        // Pengguna
+        $dataPenggunaPertama = User::where('role', 'pengguna')->orderBy('created_at')->first();
+        $bulanTahunPenggunaPertama = date('F Y', strtotime($dataPenggunaPertama->created_at));
+        $dataPenggunaTerbaru = User::where('role', 'pengguna')->orderBy('created_at')->first();
+        $bulanTahunPenggunaTerbaru = date('F Y', strtotime($dataPenggunaTerbaru->created_at));
+        // Jumlah data
+        $jumlah_Pengguna = User::where('role', 'pengguna')->count();
+
+        return view('backend.index', compact('title', 'user', 'bulanTahunPertama', 'bulanTahunTerbaru', 'jumlah_berita', 'bulanTahunOnProcessPertama', 'bulanTahunOnProcessTerbaru', 'jumlah_OnProcess', 'bulanTahunSelesaiPertama', 'bulanTahunSelesaiTerbaru', 'jumlah_Selesai', 'bulanTahunPenggunaPertama', 'bulanTahunPenggunaTerbaru', 'jumlah_Pengguna'));
     }
 }
